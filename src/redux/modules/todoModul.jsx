dd6ad518d56d097cdbd5e79bd56e058535fa7b5f
LeeSeungJin <dltmdwls154@gmail.com>
2022-08-25 21:31:37 +0900

    redux-saga Error 수정

--- src/redux/modules/todoModul.jsx
+++ src/redux/modules/todoModul.jsx
@@ -1,5 +1,54 @@
 import { createActions, handleActions } from 'redux-actions';
 
+//export const ADD_TODO = 'redux/todos/ADD_TODO';
+// export const EDIT_TODO = 'redux/todos/EDIT_TODO';
+// export const DEL_TODO = 'redux/todos/DEL_TODO';
+// export const COMPLETE_TODO = 'redux/todos/COMPLETE_TODO';
+// const initialState = [];
+//export const addTodo = (text) => {
+//   return {
+//     type: ADD_TODO,
+//     text,
+//   };
+// };
+//
+// export const completeTodo = (index) => {
+//   return {
+//     type: COMPLETE_TODO,
+//   };
+// };
+// export const editTodo = (text) => {
+//   return {
+//     type: EDIT_TODO,
+//     text,
+//   };
+// };
+// export const delTodo = (text) => {
+//   return {
+//     type: DEL_TODO,
+//     text,
+//   };
+// };
+
+//export default function todos(prevState = initialState, action) {
+//   if (action.type === ADD_TODO) {
+//     return [...prevState, { text: action.text, done: false }];
+//   }
+//   if (action.type === EDIT_TODO) {
+//   }
+//   if (action.type === DEL_TODO) {
+//   }
+//   if (action.type === COMPLETE_TODO) {
+//     return prevState.map((value, index) => {
+//       if (index === action.index) {
+//         return { ...value, done: true };
+//       }
+//       return value;
+//     });
+//   }
+//   return prevState;
+// }
+
 export const { addTodo, completeTodo, editTodo, delTodo } = createActions(
   'ADD_TODO',
   'COMPLETE_TODO',
@@ -15,7 +64,6 @@ const initialState = [];
 const reducer = handleActions(
   {
     ADD_TODO: (state, action) => {
-      console.log(state, action);
       return [...state, { text: action.payload, done: false }];
     },
     COMPLETE_TODO: (state, action) => {
--- src/redux/modules/usersModul.jsx
+++ src/redux/modules/usersModul.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { put, call, takeEvery } from '@redux-saga/core/effects';
+import { put, call, takeEvery, delay } from '@redux-saga/core/effects';
 import { push } from 'connected-react-router';
 // Action Type
 
@@ -84,23 +84,22 @@ export default Users;
 
 // redux-saga
 const GET_USERS_SAGA_START = 'redux/users/GET_USERS_SAGA_START';
+
+function* getUsersSaga() {
+  try {
+    yield put(getUsersState());
+    const { data } = yield call(axios.get, 'https://api.github.com/users');
+    yield put(getUsersSuccess(data));
+  } catch (e) {
+    yield put(getUsersFail(e));
+  }
+}
 export const getUsersSagaStart = () => {
   return {
     type: GET_USERS_SAGA_START,
   };
 };
 
-async function* getUsersSaga(action) {
-  try {
-    yield put(getUsersState());
-    const res = yield call(axios.get, 'https://api.github.com/users');
-    yield put(getUsersSuccess(res.data));
-    yield put(push('/'));
-  } catch (e) {
-    yield put(getUsersFail(e));
-  }
-}
-
 export function* usersSaga() {
-  yield takeEvery(GET_USERS_SAGA_START, getUsersSaga());
+  yield takeEvery(GET_USERS_SAGA_START, getUsersSaga);
 }
